4c0a3b98313912f75b0983c3ea7e2ed729176038
Kerstin Blumenstein <dm101504@fhstp.ac.at>
2011-05-01 17:01:39 +0200

    route2

---
+++ Route2/Classes/Route2AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  Route2AppDelegate.h
+//  Route2
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface Route2AppDelegate : PhoneGapDelegate {
+}
+
+@end
+
